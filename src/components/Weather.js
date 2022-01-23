ae0d6a8872ff9c327e78b2d7d8d211f59f6e4ee2
sandip2224 <sandipan2224@gmail.com>
2022-01-23 09:08:15 +0530

    Built simple frontend UI

---
+++ src/components/Weather.js
@@ -0,0 +1,16 @@
+const Weather = () => {
+    return (
+        <div className="container py-3">
+            <div className="cards">
+                <h1>London</h1>
+                <h5 className="py-4">
+                    <i className="wi wi-day-sunny display-1"></i>
+                </h5>
+                <h1 className="py-2">25&deg;</h1>
+                <h4 className="py-3">Slow Rain</h4>
+            </div>
+        </div>
+    );
+}
+
+export default Weather;
